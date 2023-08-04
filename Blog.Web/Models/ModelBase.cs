4938f74dbbfc8298db2fc70370d547e87c3830c2
changj <cn.changji@outlook.com>
2023-08-04 17:40:13 +0800

    change ui

---
+++ Blog.Web/Models/ModelBase.cs
@@ -0,0 +1,17 @@
+﻿namespace Blog.Web.Models
+{
+    public class ModelBase
+    {
+        public int Id { get; set; }
+
+        public DateTime CreateTime { get; set; }
+
+        public int CreateBy { get; set; }
+
+        public DateTime? UpdateTime { get; set; }
+
+        public int UpdateBy { get; set; }
+
+        public bool IsDeleted { get; set; }
+    }
+}
